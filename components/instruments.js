3489f8203abdbff0778ab2fa94ded74b31b1fd64
Angramme <kacpero376@gmail.com>
2021-07-02 16:18:25 +0200

    updated instruments code with caching etc

--- components/instruments.js
+++ components/instruments.js
@@ -1,26 +1,29 @@
 import { useTheme } from "../hooks/useTheme"
+import { getSynthNames } from "../lib/instruments";
 
+import clone from 'just-clone'
 
 export default function instruments({
         style,
         instruments: [instruments, setInstruments]
     }){
     const styles = useTheme(require('../styles/instruments.module.sass'));
-    const synth_names = [
-        'pluck',
-        'kick',
-        'crash cymbal',
-    ];
+    const synth_names = getSynthNames();
+
+    const handler = (id)=>({target})=>{
+        const val = Number(target.value);
+        instruments[id] = val;
+        setInstruments(clone(instruments));
+    }
 
     return <div className={styles.container} style={style}>
         {instruments.map((inst, i)=>
         <div key={i} className={styles.instrument}>
-            <div className={styles.header}>track {i} uses</div>
+            <div className={styles.header}>track {i+1} uses</div>
             <div className={styles.body}>
-                {/* {synth_names[inst]} */}
-                <select key={i}>
-                    {synth_names.map((v, i)=>
-                    <option value={i}>{v}</option>
+                <select key={i} defaultValue={inst} onChange={handler(i)}>
+                    {synth_names.map((v, j)=>
+                    <option value={j} key={j}>{v}</option>
                     )}
                 </select>
             </div>
--- components/main.js
+++ components/main.js
@@ -5,11 +5,11 @@ import TopBar from "./topbar";
 import Instruments from "./instruments";
 
 const Tone = require('tone');
-import CTX from '../lib/global_context'
 
 import { queryToSections } from "../lib/serialization";
 import { sectionsToImgURL } from "../lib/sectionsToImgURL";
 import favicon from '../lib/favicon'
+import { init as initSynths, updateInstruments, getInstruments, getNewInstrument } from '../lib/instruments'
 
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/dist/client/router";
@@ -37,7 +37,7 @@ export default function Main(){
 
     const [editMode, setEditMode] = useState('section');
 
-    const [instruments, setInstruments] = useState([0, 1, 2]);
+    const [instruments, setInstruments] = useState(Array.from({length: 10}, (v, i)=>getNewInstrument(i)));
 
     const [sections, setSections] = useState([make_default_section()]);
     const [curSection, setCurSection] = useState(0);
@@ -48,14 +48,15 @@ export default function Main(){
     // load state from query
     useEffect(()=>{
         if(Object.keys(router.query).length <= 0) return;
-        requestAnimationFrame(()=>{
-            setSections(queryToSections(router.query));
-            setBpm(Number(router.query.bpm));
-        });
+        console.log('found state in url, proceeding to load...');
+        
+        setSections(queryToSections(router.query));
+        setBpm(Number(router.query.bpm));
+
         router.push({
             query:{}
         }, undefined, { shallow:true });
-    }, []);
+    }, [router.query]);
 
     // play / pause
     useEffect(()=>{        
@@ -81,32 +82,11 @@ export default function Main(){
 
     // init synths
     useEffect(()=>{
-        const c = CTX();
-
-        const vol = new Tone.Volume('-22').toDestination();
-        
-        c.synths = [
-            new Tone.PluckSynth().connect(vol),
-            new Tone.MembraneSynth().connect(vol),
-            new Tone.MetalSynth().connect(vol),
-        ];
-        c.triggers = [
-            (duration, time, accent)=>c.synths[0].triggerAttackRelease(accent ? 'E3' : 'E2', duration, time, 1.2),
-            (duration, time, accent)=>c.synths[1].triggerAttackRelease(accent ? 'C1' : 'F1', duration, time, accent ? 1 : 0.8),
-            (duration, time, accent)=>c.synths[2].triggerAttackRelease('C3', duration, time, 0.2),
-        ];
-
-        c.instruments = instruments.map((x)=>c.triggers[x]);
-
-        return function cleanup(){
-            c.synths.forEach(sn=>sn.dispose());
-            vol.dispose();
-        }
+        return initSynths(instruments);
     }, [instruments]);
 
     // sections to actual sound
     useEffect(()=>{
-        const c = CTX();
         const { withScale } = require('../lib/helpers');
         const scheduleSections = require('../lib/scheduleSections').default;
 
@@ -121,7 +101,7 @@ export default function Main(){
 
         scheduleSections((time, {accent, duration, track})=>{
             try{
-                c.instruments[track](duration, time, accent);
+                getInstruments()[track](duration, time, accent);
             }catch(err){
                 console.warn(err);
             }
@@ -134,14 +114,15 @@ export default function Main(){
 
     // dynamic favicon
     useEffect(()=>{
-        requestAnimationFrame(()=>favicon(sectionsToImgURL(sections)));
+        favicon(sectionsToImgURL(sections));
     }, [sections]);
     
     return <div className={styles.container}>
         <Visualisation 
             sections={[sections, setSections]} 
             curSection={[curSection, setCurSection]} 
-            defaultSection={make_default_section}/>
+            defaultSection={make_default_section}
+            editMode={[editMode, setEditMode]}/>
         <Controls
             paused={[paused, setPaused]}
             bpm={[bpm, setBpm]}
