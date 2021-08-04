b0def0344498b05d34c6eb1c2dbfff3498c16125
Angramme <kacpero376@gmail.com>
2021-08-04 21:34:59 +0200

    added PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const withPWA = require('next-pwa');
+
 const EXPORT_TO_GH_PAGES = true;
 const GH_PAGES_REPO_NAME = 'polyrhythm3'
 const basePath = process.env.NODE_ENV === 'production' && EXPORT_TO_GH_PAGES ? `/${GH_PAGES_REPO_NAME}` : '';
 
-module.exports = {
+module.exports = withPWA({
+    pwa: {
+        dest: 'public'
+    },
     basePath,
-    assetPrefix: `${basePath}/`
-};
+    assetPrefix: `${basePath}/`,
+});
