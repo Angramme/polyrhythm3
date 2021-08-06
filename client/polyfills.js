c404bdd022231e400b4176675987dee89b7884d0
Angramme <kacpero376@gmail.com>
2021-08-06 19:25:16 +0200

    added polyfills

---
+++ client/polyfills.js
@@ -0,0 +1,19 @@
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function (s) {
+    var el = this;
+
+    do {
+      if (Element.prototype.matches.call(el, s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
+console.log("loading polyfills...")
--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
 
 const EXPORT_TO_GH_PAGES = true;
-const GH_PAGES_REPO_NAME = 'polyrhythm3'
-const basePath = process.env.NODE_ENV === 'production' && EXPORT_TO_GH_PAGES ? `/${GH_PAGES_REPO_NAME}` : '';
+const GH_PAGES_REPO_NAME = "polyrhythm3";
+const basePath =
+  process.env.NODE_ENV === "production" && EXPORT_TO_GH_PAGES
+    ? `/${GH_PAGES_REPO_NAME}`
+    : "";
 
 module.exports = withPWA({
-    pwa: {
-        dest: 'public'
-    },
-    basePath,
-    assetPrefix: `${basePath}/`,
+  pwa: {
+    dest: "public",
+  },
+  basePath,
+  assetPrefix: `${basePath}/`,
+  webpack: (cfg) => {
+    const originalEntry = cfg.entry;
+    cfg.entry = async () => {
+      const entries = await originalEntry();
+      if (entries["main.js"])
+        entries["main.js"].unshift("./client/polyfills.js");
+      return entries;
+    };
+    return cfg;
+  },
 });
