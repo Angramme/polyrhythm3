f365115c4082b36b3ca6ff223854e6754319d98c
Angramme <kacpero376@gmail.com>
2023-05-18 18:53:31 +0200

    added pwa caching

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 const basePath = "";
 
 module.exports = withPWA({
   dest: "public",
+  runtimeCaching,
 })({
   basePath,
   assetPrefix: `${basePath}/`,
