955d539a2c4584dcf6635794fb64aed128f3db0f
Angramme <34948473+Angramme@users.noreply.github.com>
2024-04-27 18:06:18 +0200

    fix metrics again

--- components/metrics/MicrosoftClarity.jsx
+++ components/metrics/MicrosoftClarity.jsx
@@ -3,14 +3,22 @@
 import Script from "next/script"
 
 export const MicrosoftClarity = () => {
-    return <Script id="clarity-script" strategy="afterInteractive">
-        {`(function(c,l,a,r,i,t,y){
+    return <script type="text/javascript" dangerouslySetInnerHTML={{
+        __html: `(function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "m1q5zx2fms");
-        `}
-    </Script>
+        })(window, document, "clarity", "script", "m1q5zx2fms");`
+    }}>
+    </script>
+    // return <Script id="clarity-script" strategy="afterInteractive">
+    //     {`(function(c,l,a,r,i,t,y){
+    //         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    //         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    //         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    //     })(window, document, "clarity", "script", "m1q5zx2fms");
+    //     `}
+    // </Script>
     // return <Script
     //         id="microsoft-clarity-init"
     //         strategy="afterInteractive"
